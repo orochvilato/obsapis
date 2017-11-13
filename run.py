aa289ce679bb231c429634c8aac65a09d4ce9920
Olivier ROCH VILATO <olivierrochvilato@yahoo.com>
2017-11-13 06:29:01 -0400

    svg templates

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from obsapis import app
+from flask import render_template
 
-
+@app.route('/test')
+def test():
+    return render_template('svg/circofrance.svg', test="youpu")
 
 if __name__ == "__main__":
     app.run()
