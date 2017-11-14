ef019e6c88209b330d40f54f2ef4213cb2d59c85
Olivier ROCH VILATO <olivier.rochvilato@chilbp.fr>
2017-11-14 10:36:38 -0400

    svgs

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
-from obsapis import app
+# -*- coding: utf-8 -*-
+
+from obsapis import app,mdb
 from flask import render_template
+from bson import json_util
 
 @app.route('/test')
 def test():
-    return render_template('svg/circofrance.svg', test="youpu")
+    return json_util.dumps(list(mdb.amendements.find({'sort':u"Adopt\u00e9","signataires_groupes":{'$elemMatch':{'$eq':'FI'}}},{'_id':None,'numInit':1,'numAmend':1})))
+    
 
 if __name__ == "__main__":
     app.run()
