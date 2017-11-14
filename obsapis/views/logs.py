36acf35b629a783af337058c1d3e01d070db7f0f
Olivier ROCH VILATO <olivier.rochvilato@chilbp.fr>
2017-11-14 11:04:05 -0400

    no cache for logsd

--- obsapis/views/logs.py
+++ obsapis/views/logs.py
@@ -1,5 +1,5 @@
 from obsapis import app,use_cache,mdb
-from flask import request,current_app
+from flask import request,current_app,response
 from obsapis.tools import json_response,cache_function
 import re
 import datetime
@@ -8,4 +8,6 @@ from obsapis.config import cache_pages_delay
 
 @app.route('/logs')
 def logs():
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
     return current_app.send_static_file('logs.html')
