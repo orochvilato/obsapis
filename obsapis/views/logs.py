92148222c886957ba05e2dbd4dca5eedfe2edcf3
Olivier ROCH VILATO <olivier.rochvilato@chilbp.fr>
2017-11-14 11:10:17 -0400

    headers

--- obsapis/views/logs.py
+++ obsapis/views/logs.py
@@ -1,5 +1,5 @@
 from obsapis import app,use_cache,mdb
-from flask import request,current_app,response
+from flask import request,current_app,Response
 from obsapis.tools import json_response,cache_function
 import re
 import datetime
@@ -8,6 +8,7 @@ from obsapis.config import cache_pages_delay
 
 @app.route('/logs')
 def logs():
-    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
-    response.headers['Pragma'] = 'no-cache'
-    return current_app.send_static_file('logs.html')
+    resp = Response(current_app.send_static_file('logs.html'))
+    resp.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    resp.headers['Pragma'] = 'no-cache'
+    return resp
