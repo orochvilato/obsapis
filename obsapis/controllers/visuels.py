baff03e4a7a99a95872ddd22c5d814fc6ea6756d
Olivier ROCH VILATO <olivierrochvilato@yahoo.com>
2017-11-22 07:27:25 -0400

    Générator

--- obsapis/controllers/visuels.py
+++ obsapis/controllers/visuels.py
@@ -7,16 +7,23 @@ from PIL import Image,ImageFont,ImageDraw
 import StringIO
 
 def get_visuel(id,depute,regen=None):
-    tailles = [(d,len(d['depute_nom'])) for d in mdb.deputes.find({'depute_actif':True}) ]
-    tailles.sort(key=lambda x:x[1],reverse=True)
-    print tailles[:4]
-    dep = mdb.deputes.find_one({'depute_shortid':depute},{'depute_departement':1,'depute_region':1,'depute_circo':1,'depute_nom':1,'_id':None})
+    dep = mdb.deputes.find_one({'depute_shortid':depute},{'depute_naissance':1,'groupe_abrev':1,'groupe_libelle':1,'depute_departement':1,'depute_region':1,'depute_circo':1,'depute_nom':1,'_id':None})
     if not dep:
         return "nope"
     circo =  "%s / %s (999) / %se circ" % (dep['depute_region'],dep['depute_departement'],dep['depute_circo'])
-    font = ImageFont.truetype("Montserrat-Bold.ttf", 14)
-    widthfactor = int(680*(float(514)/max(450,font.getsize(circo)[0])))
-    widthfactor = 680
+    gp = "%s (%s)" % (dep['groupe_libelle'],dep['groupe_abrev'])
+    font1 = ImageFont.truetype("Montserrat-Bold.ttf", 14)
+    font2 = ImageFont.truetype("Montserrat-Bold.ttf", 21)
+    font3 = ImageFont.truetype("Montserrat-Bold.ttf", 11)
+    print font3.getsize(gp)
+    wf1 = int(680*(float(510)/font1.getsize(circo)[0]))
+    wf2 = int(680*(float(413)/font2.getsize(dep['depute_nom'])[0]))
+    wf3 = int(680*(float(457)/font3.getsize(dep['depute_naissance'])[0]))
+    wf4 = int(680*(float(320)/font3.getsize(gp)[0]))
+
+    print wf1,wf2,wf3,wf4
+
+    widthcor = 565
     path = '/'.join(app.instance_path.split('/')[:-1] +['obsapis','resources','visuels',id])
     imgpath = path+'/tous/'+depute+'.png'
     import os.path
@@ -35,7 +42,7 @@ def get_visuel(id,depute,regen=None):
     else:
         factor = 1.2
         url = "http://dev.observatoire-democratie.fr/assemblee/deputes/%s/votes" % depute
-        zone = (90,310,840,570)
+        zone = (90,310,840-(680-widthcor),570)
         size = '1200x1000'
     options.add_argument('window-size=%s' % size)
     options.add_argument('force-device-scale-factor=%0.1f' % factor)
@@ -70,7 +77,7 @@ def get_visuel(id,depute,regen=None):
         final = vis
     else:
         width, height = image2.size
-        image3 = image2.resize((widthfactor,int(widthfactor*float(height)/width)),Image.ANTIALIAS) #680
+        image3 = image2.resize((680,int(680*float(height)/width)),Image.ANTIALIAS) #680
         #image3 = image2
         vis = Image.open(path+'/share_2_1_fond.png') #.resize((1024,512))
         poster = Image.open(path+'/share_2_1_poster.png') #.resize((1024,512))
