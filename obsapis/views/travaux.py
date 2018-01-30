1ed2a38777a8bdf8a325d5365eabb4c7b3f7b7ab
Olivier ROCH VILATO <olivierrochvilato@yahoo.com>
2018-01-30 19:08:52 -0400

    Filtre documents

--- obsapis/views/travaux.py
+++ obsapis/views/travaux.py
@@ -44,6 +44,8 @@ def view_travaux():
         filters.append({'type':{'$in':['QG','QE','QOSD']}})
     elif ttype=='amendement':
         filters.append({'type':'amendement'})
+    elif ttype in ['propositiondeloi', 'propositionderesolution','rapportdinformation','rapport','avis','projetdeloi']:
+        filters.append({'type':ttype})
     elif ttype=='document':
         filters.append({'type':{'$nin':['QG','QE','QOSD','amendement']}})
 
