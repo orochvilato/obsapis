530049ba649f9a367a1a52090ee64395af2fa4d3
Olivier ROCH VILATO <olivierrochvilato@yahoo.com>
2018-08-25 10:58:49 -0400

    detail scrutin

--- obsapis/views/scrutins.py
+++ obsapis/views/scrutins.py
@@ -30,7 +30,7 @@ def scrutinscles():
         if scrutins_cles[s['scrutin_num']]['inversion'] == 'oui':
 
             positions['position'] = {'pour':'contre','contre':'pour'}.get(positions['position'],positions['position'])
-            
+
             positions['pour'] = scrutins_positions[s['scrutin_num']]['contre']
             positions['contre'] = scrutins_positions[s['scrutin_num']]['pour']
 
@@ -38,3 +38,23 @@ def scrutinscles():
                           sort=s['scrutin_sort'],dossierLibelle=dossier.replace(u'\u0092',"'"),
                           detail=scrutins_cles[s['scrutin_num']],positions=positions))
     return json_response(scles)
+
+#@cache_function(expires=cache_pages_delay)
+@app.route('/scrutins/detail/<int:num>')
+def scrutinsdetail(num):
+    scrutin = mdb.scrutins.find_one({'scrutin_num':num})
+    scrutins_cles = use_cache('scrutins_cles',lambda:getScrutinsCles(),expires=3600)
+    scrutin.update(scrutins_cles.get(num,{}))
+    positions = {}
+    for dpos in mdb.votes.find({'scrutin_num':num},{'depute_shortid':1,'groupe_abrev':1,'vote_position':1}):
+        depid = dpos['depute_shortid']
+        gp = dpos['groupe_abrev']
+        pos = dpos['vote_position']
+        if not pos in positions.keys():
+            positions[pos] = {}
+        if not gp in positions[pos].keys():
+            positions[pos][gp] = []
+        positions[pos][gp].append(depid)
+
+    scrutin['positions'] = positions
+    return json_response(scrutin)
